d9b7e1cbbd0d207e2561ca76c2d5cf3788735c26
Vinicius <viniarruda.souza@gmail.com>
2018-07-01 23:03:48 -0300

    Force use ssl

--- wp-config.php
+++ wp-config.php
@@ -84,6 +84,8 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 
+define('FORCE_SSL_ADMIN', true);
+
 /* Isto é tudo, pode parar de editar! :) */
 
 /** Caminho absoluto para o diretório WordPress. */
