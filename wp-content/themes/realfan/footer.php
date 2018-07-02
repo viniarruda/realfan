f9545afce76fe81f6791e52ee8162f0b0872c380
Vinicius <viniarruda.souza@gmail.com>
2018-07-01 22:59:16 -0300

    Fix footer

--- wp-content/themes/realfan/footer.php
+++ wp-content/themes/realfan/footer.php
@@ -2,14 +2,14 @@
 
     <div class="footer_detail">
       <a href="#" class="scroll-top">
-        <img src="<?php bloginfo('template_url'); ?>/assets/images/icon-logo.png" alt="Volte ao topo">
+        <img src="<?php bloginfo('template_url'); ?>/assets/images/realfan_headerlogo.png" alt="Volte ao topo">
       </a>
     </div>
 
     <div class="footer_content">
       <div class="footer_widget footer_logo">
         <a href="#" class="scroll-top">
-          <img src="<?php bloginfo('template_url'); ?>/assets/images/drcanabbis-tipo.png" alt="DR.CANNABIS">
+          RealFan
         </a>
       </div>
 
@@ -18,15 +18,15 @@
       </div>
 
       <div class="footer_widget">
-        <a href="mailto:contato@drcannabis.com.br">contato@drcannabis.com.br</a>
+        <a href="mailto:julioconsultorias@gmail.com">julioconsultorias@gmail.com</a>
       </div>
 
       <div class="footer_widget">
-        <a class="social-link" href="https://www.facebook.com/drcannabs/" target="_blank">
+        <a class="social-link" href="#" target="_blank">
           <img src="<?php bloginfo('template_url'); ?>/assets/images/icon-facebook.png" alt="Facebook">
         </a>
 
-        <a class="social-link" href="https://www.instagram.com/drcannabisoficial" target="_blank">
+        <a class="social-link" href="#" target="_blank">
           <img src="<?php bloginfo('template_url'); ?>/assets/images/icon-instagram.png" alt="Instagram">
         </a>
       </div>
