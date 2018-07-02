b317c34becc4fee7ebcce8fa7a7d634e0874c242
Vinicius <viniarruda.souza@gmail.com>
2018-07-01 22:32:58 -0300

    Css

--- wp-content/themes/realfan/header.php
+++ wp-content/themes/realfan/header.php
@@ -5,7 +5,7 @@
     <title><?php wp_title( '|', true, 'right' ); ?> <?php bloginfo('name'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
-    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/styles/all.css?v=2.1">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/styles/all.css">
 
     <!-- Facebook Share -->
     <?php if(is_single() || is_page()) { ?>
